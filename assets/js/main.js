45ab81db1cb6dbcf377d752d3061f38d4f8bf5cf
kmthein <kmthein96.mm@gmail.com>
2024-04-12 14:20:38 +0630

    popup done

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,12 +1,16 @@
 const loginBtn = document.querySelector(".main .login-btn");
+const closeBtn = document.querySelector(".close-btn");
 const background = document.querySelector(".modal-overlay");
 const modal = document.querySelector(".modal-container");
+
 
 loginBtn.addEventListener("click", (e) => {
     modal.classList.add("show");
 })
-
 background.addEventListener("click", (e) => {
+    modal.classList.remove("show");
+})  
+closeBtn.addEventListener("click", (e) => {
     modal.classList.remove("show");
 })  
 
